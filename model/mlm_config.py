39ebb5df1fb90fc2b3de388e3ee70db0ad0351fa
Ollie Broadhurst <olliebroadhurst@gmail.com>
2022-11-21 11:12:22 +0200

    Add adapter config

--- model/mlm_config.py
+++ model/mlm_config.py
@@ -58,13 +58,12 @@ class CTCDecoderConfig(PretrainedConfig):
 
     def __init__(
         self,
-        vocab_size=32,
-        decoder_vocab_size=30522,
+        vocab_size=32,        
         max_position_embeddings=512,
         sinusoidal_pos_embds=False,
         n_layers=6,
         n_heads=12,
-        dim=768,
+        dim=768,        
         hidden_dim=4 * 768,
         dropout=0.1,
         attention_dropout=0.1,
@@ -73,17 +72,20 @@ class CTCDecoderConfig(PretrainedConfig):
         qa_dropout=0.1,
         seq_classif_dropout=0.2,
         pad_token_id=0,
+        decoder_vocab_size=30522,
+        output_hidden_size=768,
+        num_adapter_layers=2,
+        adapter_stride=2,
         ctc_loss_reduction="mean",
         ctc_zero_infinity=True,
         **kwargs
     ):
-        self.vocab_size = vocab_size
-        self.decoder_vocab_size = decoder_vocab_size
+        self.vocab_size = vocab_size        
         self.max_position_embeddings = max_position_embeddings
         self.sinusoidal_pos_embds = sinusoidal_pos_embds
         self.n_layers = n_layers
         self.n_heads = n_heads
-        self.dim = dim
+        self.dim = dim        
         self.hidden_dim = hidden_dim
         self.dropout = dropout
         self.attention_dropout = attention_dropout
@@ -91,6 +93,11 @@ class CTCDecoderConfig(PretrainedConfig):
         self.initializer_range = initializer_range
         self.qa_dropout = qa_dropout
         self.seq_classif_dropout = seq_classif_dropout
+        self.decoder_vocab_size = decoder_vocab_size
+        self.output_hidden_size = output_hidden_size
+        self.num_adapter_layers = num_adapter_layers
+        self.layerdrop = dropout
+        self.adapter_stride = adapter_stride
         self.ctc_loss_reduction = ctc_loss_reduction
         self.ctc_zero_infinity = ctc_zero_infinity
         super().__init__(**kwargs, pad_token_id=pad_token_id)
